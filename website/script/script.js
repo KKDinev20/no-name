46610dca753d887d6ef5005e952ac92c93041f14
vvbolashikova20 <vvbolashikova20@codingburgas.bg>
2022-02-23 19:30:17 +0200

    Fixing some issues

--- website/script/script.js
+++ website/script/script.js
@@ -1,15 +1,14 @@
 function openNav() {
     if (window.innerWidth <= 700) {
       document.getElementById("nav").style.width = "100%";
+      document.getElementById("main").style.marginLeft = "0px";
     } else {
       document.getElementById("nav").style.width = "250px";
       document.getElementById("main").style.marginLeft = "250px";
     }
-
-    document.getElementById("menu-toggle").style.backgroundColor= "#94af97";
+    
+    window.addEventListener("resize", openNav);
 }
-
-window.addEventListener("resize", openNav);
 
 function closeNav() {
     if (window.innerWidth <= 700) {
@@ -18,6 +17,4 @@ function closeNav() {
       document.getElementById("nav").style.width = "0";
       document.getElementById("main").style.marginLeft= "0";
     }
-
-    document.getElementById("menu-toggle").style.backgroundColor= "#c6f1ca";
 }
