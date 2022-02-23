4b759e4dd693a12a4474924389022fde04b1e133
vvbolashikova20 <vvbolashikova20@codingburgas.bg>
2022-02-23 17:32:11 +0200

    Modify the navbar

--- website/script/script.js
+++ website/script/script.js
@@ -1,19 +1,23 @@
 function openNav() {
     if (window.innerWidth <= 700) {
-      document.getElementById("sidenav").style.width = "100%";
+      document.getElementById("nav").style.width = "100%";
     } else {
-      document.getElementById("sidenav").style.width = "250px";
+      document.getElementById("nav").style.width = "250px";
       document.getElementById("main").style.marginLeft = "250px";
     }
+
     document.getElementById("menu-toggle").style.backgroundColor= "#94af97";
 }
-  
+
+window.addEventListener("resize", openNav);
+
 function closeNav() {
     if (window.innerWidth <= 700) {
-      document.getElementById("sidenav").style.width = "0";
+      document.getElementById("nav").style.width = "0";
     } else {
-      document.getElementById("sidenav").style.width = "0";
+      document.getElementById("nav").style.width = "0";
       document.getElementById("main").style.marginLeft= "0";
     }
+
     document.getElementById("menu-toggle").style.backgroundColor= "#c6f1ca";
 }
